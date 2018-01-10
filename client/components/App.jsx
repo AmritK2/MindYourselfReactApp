63c19f3018c2d84d756409f9a75e8bc1f7457787
AmritK2 <amrit.kaur@myob.com>
2018-01-11 11:35:09 +1300

    takes in user input but fails in getting the data

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -7,16 +7,65 @@ import CodeSchoolData from './APICodeSchool.jsx';
 import GithubUserData from './APIGithub.jsx';
 
 class App extends React.Component {
-  render() {
-    return (
-     <div style={{textAlign: 'center'}}>
-        <h1>Intern Pack</h1>
-         <CodeWarsData username = "Amrit2" />
-         <CodeSchoolData user = "AmritK2"/>
-         <GithubUserData user = "AmritK2"/>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            codeWarsName: '',
+            codeSchoolName: '',
+            gitHubName: ''
+        };
+
+    }
+
+    handleCWChange(e) {
+        this.setState({...this.state, codeWarsName: e.target.value});
+    }
+
+    handleCSChange(e) {
+        this.setState({...this.state, codeSchoolName: e.target.value});
+    }
+
+    handleGitChange(e) {
+        this.setState({...this.state, gitHubName: e.target.value});
+    }
+
+    render() {
+        return (
+
+            <form>
+                <h1>Intern Pack</h1>
+
+                <h2> Please enter your usernames below</h2>
+
+                <label>
+                    Code Wars Username:
+                </label>
+                <input type="name" onChange={this.handleCWChange.bind(this)} value={this.state.codeWarsName}/>
+                <br />
+
+                <label>
+                    Code School Username:
+                </label>
+                <input type="name" onChange={this.handleCSChange.bind(this)} value={this.state.codeSchoolName}/>
+                <br />
+
+                <label>
+                    GitHub Username:
+                </label>
+                <input type="name" onChange={this.handleGitChange.bind(this)} value={this.state.gitHubName}/>
+                <br />
+
+                <button type = "submit"> Submit </button>
+                <div>
+                    <CodeWarsData username={this.state.codeWarsName}/>
+                    <CodeSchoolData user={this.state.codeSchoolName}/>
+                    <GithubUserData user={this.state.gitHubName}/>
+                </div>
+            </form>
+
+        );
+    }
 }
+
 export default App;
 
--- webpack.config.js
+++ webpack.config.js
@@ -4,22 +4,28 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './client/index.html',
-  filename: 'index.html',
-  inject: 'body'
-})
+    template: './client/index.html',
+    filename: 'index.html',
+    inject: 'body'
+});
 module.exports = {
-  entry: './client/index.js',
-  output: {
-    path: path.resolve('dist'),
-    filename: 'index_bundle.js'
-  },
-  module: {
-    loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
-    ]
-  },
-  plugins : [HtmlWebpackPluginConfig]
-}
+    entry: './client/index.js',
+    output: {
+        path: path.resolve('dist'),
+        filename: 'index_bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ["es2015", "react", "stage-2"]
+                }
+            }
+        ]
+    },
+    plugins: [HtmlWebpackPluginConfig]
+};
     
